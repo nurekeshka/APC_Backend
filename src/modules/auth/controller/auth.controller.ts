a85d0cef2ece2ca0651a30f181098c7dfbf52cb2
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-11-20 20:16:22 +0100

    feat: add auth module documentation

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -6,22 +6,56 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 import { SignInDTO } from '../dto/sign-in.dto';
 import { AuthGuard } from '../guards/auth.guard';
 import { AuthService } from '../service/auth.service';
 
+@ApiTags('Auth Controller')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @ApiResponse({
+    status: 201,
+    description: 'Returns Access Token',
+    example: {
+      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Email and Password mismatch',
+    example: {
+      message: 'Unauthorized',
+      statusCode: 401,
+    },
+  })
   signIn(@Body() dto: SignInDTO) {
     return this.authService.signIn(dto.email, dto.password);
   }
 
   @Get('profile')
   @UseGuards(AuthGuard)
+  @ApiResponse({
+    status: 200,
+    description: 'Returns Profile',
+    example: {
+      email: 'example@api.com',
+      iat: 1732127822,
+      exp: 1732131422,
+    },
+  })
+  @ApiResponse({
+    status: 401,
+    description: 'Email and Password mismatch',
+    example: {
+      message: 'Unauthorized',
+      statusCode: 401,
+    },
+  })
   getProfile(@Request() request: any) {
     return request.user;
   }
--- src/modules/auth/dto/sign-in.dto.ts
+++ src/modules/auth/dto/sign-in.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInDTO {
+  @ApiProperty({ example: 'example@api.com', required: true })
   @IsEmail()
   email: string;
 
+  @ApiProperty({ example: 'Strong123!', required: true })
   @IsString()
   @IsNotEmpty()
   password: string;
