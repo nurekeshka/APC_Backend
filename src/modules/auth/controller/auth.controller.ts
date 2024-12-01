880a0199c87206db4ffbd55d3fb50f8e31a38293
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-12-01 02:17:20 +0100

    fix: remove swagger tags

--- src/modules/auth/controller/auth.controller.ts
+++ src/modules/auth/controller/auth.controller.ts
@@ -6,7 +6,7 @@ import {
   Request,
   UseGuards,
 } from '@nestjs/common';
-import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
 import { SignInDTO } from '../dto/sign-in.dto';
 import { AuthGuard } from '../guards/auth.guard';
@@ -18,41 +18,12 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('sign-in')
-  @ApiResponse({
-    status: 201,
-    example: {
-      access_token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9',
-    },
-  })
-  @ApiResponse({
-    status: 401,
-    example: {
-      message: 'Unauthorized',
-      statusCode: 401,
-    },
-  })
   signIn(@Body() dto: SignInDTO) {
     return this.authService.signIn(dto.email, dto.password);
   }
 
   @Get('profile')
   @UseGuards(AuthGuard)
-  @ApiBearerAuth()
-  @ApiResponse({
-    status: 200,
-    example: {
-      email: 'example@api.com',
-      iat: 1732127822,
-      exp: 1732131422,
-    },
-  })
-  @ApiResponse({
-    status: 401,
-    example: {
-      message: 'Unauthorized',
-      statusCode: 401,
-    },
-  })
   getProfile(@Request() request: any) {
     return request.user;
   }
