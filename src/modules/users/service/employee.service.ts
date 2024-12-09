4a4453f1734240bbb9923bf41c5679d9bc612b1e
Nurbek <89794613+nurekeshka@users.noreply.github.com>
2024-12-09 17:43:13 +0100

    fix: conflicts with database branch

--- src/modules/users/service/employee.service.ts
+++ src/modules/users/service/employee.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { Column } from 'nestjs-paginate/lib/helper';
 
+import { OperationsDtos } from '../../../common/operations/decorators/operations.dtos.decorator';
 import { OperationsEntity } from '../../../common/operations/decorators/operations.entity.decorator';
 import { OperationsService } from '../../../common/operations/service/operations.service';
 import { CreateEmployeeDto } from '../dto/create-employee.dto';
+import { UpdateEmployeeDto } from '../dto/update-employee.dto';
 import { Employee } from '../entities/employee.entity';
 
 @Injectable()
 @OperationsEntity({ entity: Employee })
+@OperationsDtos({ create: CreateEmployeeDto, update: UpdateEmployeeDto })
 export class EmployeeService extends OperationsService<
   Employee,
   CreateEmployeeDto
